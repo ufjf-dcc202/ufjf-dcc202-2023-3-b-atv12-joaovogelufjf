6a998c0bede91f7fc014e735570d424b46c8f7a8
João Vítor Vogel <joao.vogel@estudante.ufjf.br>
2023-11-28 22:08:36 -0300

    formulario com nome entrada

---
+++ main.js
@@ -0,0 +1,11 @@
+document.entrada.enviar.addEventListener('submit', leFormulario);
+
+function leFormulario(event) {
+
+    event.preventDefault();
+    const quantidade = document.entrada.quantidade.value;
+    const fruta = document.entrada.fruta.value;
+
+    console.log(`Eu tenho ${quantidade} ${fruta}`);
+
+}
